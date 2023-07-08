1b6c87f383e793982d86d0a0afd0a4cca3dc558d
Sage Drake <sagedrake11@gmail.com>
2023-07-08 16:21:31 -0700

    Style new post editor

--- content-script.js
+++ content-script.js
@@ -12,18 +12,20 @@
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log('HELLO');
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
         if (request.greeting === "dark") {
             const cssLink = document.createElement("link");
             cssLink.href = chrome.runtime.getURL("editingPaneStyling.css");
             cssLink.rel = "stylesheet";
             cssLink.type = "text/css";
 
-            console.log(document.getElementById('s_answer_edit_ifr').contentWindow.document);
-            document.getElementById('s_answer_edit_ifr').contentWindow.document.head.appendChild(cssLink);
+            if (document.getElementById('s_answer_edit_ifr') != null) {
+                document.getElementById('s_answer_edit_ifr').contentWindow.document.head.appendChild(cssLink);
+            }
+
+            if (document.getElementById('create_post_edit_ifr') != null) {
+                document.getElementById('create_post_edit_ifr').contentWindow.document.head.appendChild(cssLink);
+            }
+
             sendResponse({farewell: "goodbye"});
         }
     }
