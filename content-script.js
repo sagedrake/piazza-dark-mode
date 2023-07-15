3ed3e0137a8ccf3f956bd3dbe7c9fcdcbcadd66d
Sage Drake <sagedrake11@gmail.com>
2023-07-15 16:30:14 -0700

    Whiten text in any iframe once it's created

--- content-script.js
+++ content-script.js
@@ -1,27 +1,65 @@
+console.log('heyyyy');
+
+
+// wait 1 second, then add event listeners to elements that will open editing panes when clicked
+// te 1-second delay is needed because these elements might not exist until some milliseconds after the site loads
+setTimeout( function() {
+    if (document.getElementById('s_answerPlaceholderId') != null) {
+        document.getElementById('s_answerPlaceholderId').addEventListener("mousedown", clicked);
+    }
+    if (document.getElementById('followup-box') != null) {
+        document.getElementById('followup-box').addEventListener("mousedown", clicked);
+    }
+    let replyButtons = document.querySelectorAll('input[data-id^="followup_reply_id"]');
+    replyButtons.forEach(function(button) {
+        button.addEventListener('mousedown', clicked);
+    });
+}, 1000)
+
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.greeting === "dark") {
-
-            const cssLink = document.createElement("link");
-            cssLink.href = chrome.runtime.getURL("editingPaneStyling.css");
-            cssLink.rel = "stylesheet";
-            cssLink.type = "text/css";
-
-            console.log('hello, I can talk');
-
-            if (document.getElementById('s_answer_edit_ifr') != null) {
-                document.getElementById('s_answer_edit_ifr').contentWindow.document.head.appendChild(cssLink);
-            }
-
-            if (document.getElementById('create_post_edit_ifr') != null) {
-                document.getElementById('create_post_edit_ifr').contentWindow.document.head.appendChild(cssLink);
-            }
-
-            if (document.getElementById('s_answerPlaceholderId') != null) {
-                console.log('the thing exists');
-            }
-
+            setTimeout(function() {
+                darkenEditingPanes();
+            }, 100);
+            setTimeout(function() {
+                darkenEditingPanes();
+            }, 1000);
             sendResponse({farewell: "goodbye"});
         }
     }
 );
+
+function clicked() {
+    setTimeout(function() {
+        darkenEditingPanes();
+    }, 100);
+    setTimeout(function() {
+        darkenEditingPanes();
+    }, 1000);
+}
+
+function darkenEditingPanes() {
+    const cssLink = document.createElement("link");
+    cssLink.href = chrome.runtime.getURL("editingPaneStyling.css");
+    cssLink.rel = "stylesheet";
+    cssLink.type = "text/css";
+
+    if (document.getElementById('s_answer_edit_ifr') != null) {
+        document.getElementById('s_answer_edit_ifr').contentWindow.document.head.appendChild(cssLink);
+    }
+
+    if (document.getElementById('create_post_edit_ifr') != null) {
+        document.getElementById('create_post_edit_ifr').contentWindow.document.head.appendChild(cssLink);
+    }
+
+    if (document.getElementById('create_followup_ifr') != null) {
+        document.getElementById('create_followup_ifr').contentWindow.document.head.appendChild(cssLink);
+    }
+
+    let replyEditors = document.querySelectorAll('[id^="create_followup_reply_"][id$="_ifr"]');
+    replyEditors.forEach(function(editor) {
+        editor.contentWindow.document.head.appendChild(cssLink);
+    });
+}
