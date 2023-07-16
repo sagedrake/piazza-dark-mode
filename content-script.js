f2c74505ab23592040c69dc85e26e1b83740865a
Sage Drake <sagedrake11@gmail.com>
2023-07-16 15:18:56 -0700

    Whiten iframe text when "edit" button clicked

--- content-script.js
+++ content-script.js
@@ -1,20 +1,4 @@
-function addListeners() {
-    // wait 1 second, then add event listeners to elements that will open editing panes when clicked
-    // the 1-second delay is needed because these elements might not exist until some milliseconds after the site loads
-    setTimeout( function() {
-        if (document.getElementById('s_answerPlaceholderId') != null) {
-            document.getElementById('s_answerPlaceholderId').addEventListener("mousedown", clicked);
-        }
-        if (document.getElementById('followup-box') != null) {
-            document.getElementById('followup-box').addEventListener("mousedown", clicked);
-        }
-        let replyButtons = document.querySelectorAll('input[data-id^="followup_reply_id"]');
-        replyButtons.forEach(function(button) {
-            button.addEventListener('mousedown', clicked);
-        });
-    }, 1000)
-}
-
+// runs when piazza site loads
 chrome.storage.local.get(["mode"]).then( (result) => {
     // set toggle to be initially on if mode is currently set to DARK
     if (result.mode === "DARK") {
@@ -22,7 +6,31 @@ chrome.storage.local.get(["mode"]).then( (result) => {
     }
 });
 
+function addListeners() {
+    // wait 1 second, then add event listeners to elements that will open editing panes when clicked
+    // the 1-second delay is needed because these elements might not exist until some milliseconds after the site loads
+    setTimeout( function() {
+        if (document.getElementById('s_answerPlaceholderId') != null) {
+            document.getElementById('s_answerPlaceholderId').addEventListener("mousedown", editingPaneInitializerClicked);
+        }
+        if (document.getElementById('followup-box') != null) {
+            document.getElementById('followup-box').addEventListener("mousedown", editingPaneInitializerClicked);
+        }
+        let replyButtons = document.querySelectorAll('input[data-id^="followup_reply_id"]');
+        replyButtons.forEach(function(button) {
+            button.addEventListener('mousedown', editingPaneInitializerClicked);
+        });
 
+        let editButtons = document.querySelectorAll('button[data-id="edit_button"]');
+        editButtons.forEach(function(button) {
+            button.addEventListener('click', editingPaneInitializerClicked);
+        });
+
+    }, 1000)
+}
+
+
+// runs when message received from background.js or popup.js
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.greeting === "dark") {
@@ -37,7 +45,8 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
-function clicked() {
+function editingPaneInitializerClicked() {
+    console.log('editing button clicked');
     setTimeout(function() {
         darkenEditingPanes();
     }, 100);
@@ -62,6 +71,10 @@ function darkenEditingPanes() {
 
     if (document.getElementById('create_followup_ifr') != null) {
         document.getElementById('create_followup_ifr').contentWindow.document.head.appendChild(cssLink);
+    }
+
+    if (document.getElementById('edit_post_ifr') != null) {
+        document.getElementById('edit_post_ifr').contentWindow.document.head.appendChild(cssLink);
     }
 
     let replyEditors = document.querySelectorAll('[id^="create_followup_reply_"][id$="_ifr"]');
